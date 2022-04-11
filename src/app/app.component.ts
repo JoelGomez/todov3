26e4d6c4d300e02b42ba7f3a03ad85ee0f885974
JoelGomez <joel.lgr@gmail.com>
2022-04-10 21:55:12 -0600

    interfaz gráfica versión mobil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCircleRight, faCoffee, faFloppyDisk, faSave, faSquareCaretRight, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todov3';
+  save = faFloppyDisk;
+  trashcan = faTrash;
+  next = faCircleRight;
 }
