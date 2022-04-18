2a687c8bc93ba3be5b854797a6e96181c314e745
JoelGomez <joel.lgr@gmail.com>
2022-04-17 20:11:08 -0600

    se agrega funcionalidad de angular y se reparan algunos bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,58 @@ export class AppComponent {
   trashcan = faTrash;
   next = faCircleRight;
   prev = faCircleLeft;
+
+  todoTasks: string[] = [];
+  todoNewTask: string = '';
+
+  inprogressTasks: string[] = [];
+  inprogressNewTask: string = '';
+
+  doneTasks: string[] = [];
+  doneNewTask: string = '';
+
+  addTodoTask(){
+    this.todoTasks.push(this.todoNewTask);
+    this.todoNewTask = '';
+  }
+
+  moveTodoTask(index: number){
+    this.inprogressNewTask = this.todoTasks[index];
+    this.todoTasks.splice(index,1);
+    this.inprogressTasks.push(this.inprogressNewTask);
+  }
+
+  moveInProgress(index: number){
+    this.inprogressNewTask = this.doneTasks[index];
+    this.inprogressTasks.push(this.inprogressNewTask);
+    this.doneTasks.splice(index,1);
+  }
+
+  deleteTask(index: number){
+    this.doneTasks.splice(index,1);
+  }
+
+  deleteTaskInProgress(index: number){
+    this.inprogressTasks.splice(index,1);
+  }
+
+  deleteTaskToDo(index: number){
+    this.todoTasks.splice(index,1);
+  }
+
+  moveTodo(index: number){
+    this.todoNewTask = this.inprogressTasks[index];
+    this.todoTasks.push(this.todoNewTask);
+    this.inprogressTasks.splice(index,1);
+  }
+
+  moveDone(index: number){
+    this.doneNewTask = this.inprogressTasks[index];
+    this.doneTasks.push(this.doneNewTask);
+    this.inprogressTasks.splice(index,1);
+  }
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -10,7 +11,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
